1684874036ff8104381c94f2268b537f889fe431
bee <418183384@qq.com>
2017-04-20 16:40:33 +0800

    补充

---
+++ src/main/java/sample/controller/ybzf/YBZFListener.java
@@ -0,0 +1,9 @@
+package sample.controller.ybzf;
+
+/**
+ * Created by Bee on 2017/4/19.
+ */
+public interface YBZFListener {
+    public void btnClickWithText(String str);
+    public void okBtnClick();
+}
